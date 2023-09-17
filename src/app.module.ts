939d80ca01b646f9dfe98bc2ee3c15069d995c71
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-09-17 11:17:48 -0500

    add dtos and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PatientsModule } from './patients/patients.module';
 import { DoctorsModule } from './doctors/doctors.module';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { DoctorsModule } from './doctors/doctors.module';
     }),
     PatientsModule,
     DoctorsModule,
+    AppointmentsModule,
   ],
   controllers: [],
   providers: [],
