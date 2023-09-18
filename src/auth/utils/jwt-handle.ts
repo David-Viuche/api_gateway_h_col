412720d135c7ce9273e5fab4a91d3711385ca9fe
David-Viuche <jviucheroja@uniminuto.edu.co>
2023-09-17 21:49:03 -0500

    add jwt handle

---
+++ src/auth/utils/jwt-handle.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtHandle {
+  constructor(private jwtService: JwtService) { }
+
+  public getIdByToken(token: string) {
+    const response = this.jwtService.verify(token);
+    return response;
+  }
+}
